e0472a316c77e31443865b8ceaae5f030839de53
mldsveda <86206345+mldsveda@users.noreply.github.com>
2022-02-26 23:29:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    py_modules=["PyScrappy", "alibaba", "amazon", "flipkart", "image", "imdb", "instagram", "news", "snapdeal", "soundcloud", "spotify", "stock", "swiggy", "twitter", "wikipedia", "youtube", "zomato"],
+    py_modules=["PyScrappy", "alibaba", "amazon", "flipkart", "image", "imdb", "instagram", "linkedin", "news", "snapdeal", "soundcloud", "spotify", "stock", "swiggy", "twitter", "wikipedia", "youtube", "zomato"],
     package_dir={"": "src"},
     install_requires=[
         'selenium',
