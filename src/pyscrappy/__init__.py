9deb634d1f1b3c3aa969a06645d6f77a4a1b88ed
Vedaant Singh <vedaantsingh706@gmail.com>
2026-07-26 11:10:03 +0100

    Version Bump

--- src/pyscrappy/__init__.py
+++ src/pyscrappy/__init__.py
@@ -62,7 +62,7 @@ from pyscrappy.scrapers.wikipedia import WikipediaScraper
 from pyscrappy.scrapers.youtube import YouTubeScraper
 from pyscrappy.scrapers.zomato import ZomatoScraper
 
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 
 __all__ = [
     # Core
